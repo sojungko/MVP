a94525b21ba9e46a7cf41dbfe5ed0dcbd5b487fa
Sojung Park <sojungko@gmail.com>
2016-11-29 11:56:42 -0500

    Remove unnecessary code for testing purposes

--- client/app/upload/upload.js
+++ client/app/upload/upload.js
@@ -4,23 +4,23 @@ angular.module('image-upload', [])
     $scope.submit = function() {
       console.log("HIT SUBMIT")
       Uploads.addOne($scope.image)
-        .then(function(res) {
-          console.log('UPLOADCONTROLLER IMAGE : ', res.data);
-          $scope.image = res.data.originalname;
-        })
-        .catch(function(err) {
-          console.log('Error posting image : ', err);
-        });
+        // .then(function(res) {
+        //   console.log('UPLOADCONTROLLER IMAGE : ', res.data);
+        //   $scope.image = res.data.originalname;
+        // })
+        // .catch(function(err) {
+        //   console.log('Error posting image : ', err);
+        // });
     };
-    $scope.readFile = function(elem) {
-      var file = elem.files[0];
-      var reader= new FileReader();
-      reader.onload = function() {
-        $scope.$apply(function() {
-          $scope.file = file;
-          $scope.imageUrl = reader.result;
-        })
-      }
-      reader.readAsDataURL(file);
-    }
+    // $scope.readFile = function(elem) {
+    //   var file = elem.files[0];
+    //   var reader= new FileReader();
+    //   reader.onload = function() {
+    //     $scope.$apply(function() {
+    //       $scope.file = file;
+    //       $scope.imageUrl = reader.result;
+    //     })
+    //   }
+    //   reader.readAsDataURL(file);
+    // }
 })
